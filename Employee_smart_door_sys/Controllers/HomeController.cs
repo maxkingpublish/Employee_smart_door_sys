4fbb14a6f47e3e0f241c2899e0f09429ca607c39
maxkingpublish <maxkingpublish@gmail.com>
2020-02-19 22:38:45 +0800

    first commit

--- Employee_smart_door_sys/Controllers/HomeController.cs
+++ Employee_smart_door_sys/Controllers/HomeController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Employee_smart_door_sys.Models;
 
 namespace Employee_smart_door_sys.Controllers
 {
     public class HomeController : Controller
     {
+        ep_listEntities db = new ep_listEntities();
         public ActionResult Index()
         {
+          
             return View();
         }
+        public ActionResult employee_list()
+        {
+
+            var ep_list = db.ep_Table.OrderByDescending(m => m.ep_Id).ToList();
+
+            return View(ep_list);
+        }
+
 
         public ActionResult About()
         {
